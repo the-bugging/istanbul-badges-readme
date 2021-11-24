d96ddf586bdad436a4be472dc2ba0ce66e014457
Micael Levi (lab) <mllc@icomp.ufam.edu.br>
2021-11-24 19:32:12 -0400

    test: fix and add more tests for `validate` module

--- tests/validate.spec.ts
+++ tests/validate.spec.ts
@@ -1,6 +1,7 @@
+import fs from 'fs';
 import path from 'path';
 import {
-  doesReadmeFileExist,
+  doesReadmeFileExistWithRightPermissions,
   doesCoverageFileExist,
   doesCoverageHashesExist,
   doesReadmeHashExist,
@@ -11,10 +12,73 @@ describe('Tests validate file', () => {
     jest.clearAllMocks();
   });
 
-  it('should throw when doesReadmeFileExist does not find file', async () => {
-    await doesReadmeFileExist('fake-path').catch((error) => {
-      expect(error).toEqual('Readme file does not exist');
+  it('should throw when doesReadmeFileExistWithRightPermissions does not find file', async () => {
+    const fsSpy = jest.spyOn(fs, 'accessSync').mockImplementation(() => {
+      const error: NodeJS.ErrnoException = new Error();
+      error.code = 'ENOENT';
+      throw error;
     });
+
+    await expect(
+      //
+      doesReadmeFileExistWithRightPermissions('fake-path'),
+    ).rejects.toEqual('Readme file does not exist');
+
+    expect(fsSpy).toHaveBeenNthCalledWith(1, 'fake-path', fs.constants.R_OK | fs.constants.W_OK);
+  });
+
+  it('should throw when doesReadmeFileExistWithRightPermissions does find file the file but it does not have read & write permissions', async () => {
+    const fsMock = jest.spyOn(fs, 'accessSync').mockImplementation(() => {
+      const error: NodeJS.ErrnoException = new Error();
+      error.code = 'EACCES';
+      throw error;
+    });
+
+    await expect(
+      //
+      doesReadmeFileExistWithRightPermissions('fake-path'),
+    ).rejects.toEqual('Readme file does not have read and write permissions');
+
+    expect(fsMock).toHaveBeenNthCalledWith(1, 'fake-path', fs.constants.R_OK | fs.constants.W_OK);
+  });
+
+  test('doesReadmeFileExistWithRightPermissions should rethrow any other fs error there is not related with file permissions nor find issues', async () => {
+    const fsSpy = jest.spyOn(fs, 'accessSync').mockImplementation(() => {
+      const error: NodeJS.ErrnoException = new Error('something');
+      error.code = 'EBUSY';
+      throw error;
+    });
+
+    await expect(
+      //
+      doesReadmeFileExistWithRightPermissions('fake-path'),
+    ).rejects.toEqual('something');
+
+    expect(fsSpy).toHaveBeenNthCalledWith(1, 'fake-path', fs.constants.R_OK | fs.constants.W_OK);
+  });
+
+  test('doesReadmeFileExistWithRightPermissions should rethrow any other error there is not related with fs module', async () => {
+    const fsSpy = jest.spyOn(fs, 'accessSync');
+
+    fsSpy.mockImplementation(() => {
+      throw new Error('something');
+    });
+
+    await expect(
+      //
+      doesReadmeFileExistWithRightPermissions('fake-path'),
+    ).rejects.toEqual('something');
+
+    fsSpy.mockImplementation(() => {
+      throw new Error();
+    });
+
+    await expect(
+      //
+      doesReadmeFileExistWithRightPermissions('fake-path'),
+    ).rejects.toEqual('Something went wrong');
+
+    expect(fsSpy).toHaveBeenNthCalledWith(2, 'fake-path', fs.constants.R_OK | fs.constants.W_OK);
   });
 
   it('should throw when doesCoverageFileExist does not find file', async () => {
